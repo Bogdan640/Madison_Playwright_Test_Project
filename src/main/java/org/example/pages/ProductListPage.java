37d1d9c611de493a85cf360d5b2a265d3c31418d
Bogdan640 <bogdanbubu2004@gmail.com>
2025-08-12 16:56:14 +0300

    added a method in product list to check sth, commit and push as the end of the day :0

--- src/main/java/org/example/pages/ProductListPage.java
+++ src/main/java/org/example/pages/ProductListPage.java
@@ -7,16 +7,24 @@ import com.microsoft.playwright.options.AriaRole;
 public class ProductListPage{
     private Page page;
     private final Locator addToCompareButton;
+    private final String productName = "samponel";
+    private final Locator viewProductPageByProductName;
 
 
     public ProductListPage(Page page) {
         this.page = page;
         addToCompareButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Compare"));
+        viewProductPageByProductName = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Samponel")).getByRole(AriaRole.LINK);
     }
+
+
 
     public void clickCompareButton() {
         addToCompareButton.click();
     }
 
-    //TODO: create click product
+    public void clickViewProductPageByProductName() {
+        viewProductPageByProductName.click();
+    }
+
 }
